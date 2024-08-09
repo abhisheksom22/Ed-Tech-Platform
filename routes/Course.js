7efdb6864f9937f6720294a1f294e402796eb2aa
Abhishek Som <abhisheksom22@gmail.com>
2024-08-09 11:39:28 +0530

    changes in course route

--- routes/Course.js
+++ routes/Course.js
@@ -70,10 +70,9 @@ router.post("/getCourseDetails", getCourseDetails)
 //                                      Category routes (Only by Admin)
 // ********************************************************************************************************
 // Category can Only be Created by Admin
-// TODO: Put IsAdmin Middleware here
 router.post("/createCategory", auth, isAdmin, createCategory)
-router.get("/showAllCategories", showAllCategories)
-router.post("/getCategoryPageDetails", categoryPageDetails)
+router.get("/showAllCategories", isAdmin, showAllCategories)
+router.post("/getCategoryPageDetails", isAdmin, categoryPageDetails)
 
 // ********************************************************************************************************
 //                                      Rating and Review
